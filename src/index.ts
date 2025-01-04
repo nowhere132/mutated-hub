364d6ca82072066240d05d5139475f3ac95f5969
nowhere132 <canh.manh123@gmail.com>
2025-01-04 21:26:13 +0700

    [nowhere132][be] Define CORS options

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import express from 'express';
+import cors from 'cors';
 
 const PORT = process.env.APP_SERVER_PORT;
 
+const corsOptions: cors.CorsOptions = {
+  origin: process.env.SECURE_CORS_ORIGIN,
+  methods: process.env.SECURE_CORS_METHODS,
+  allowedHeaders: process.env.SECURE_CORS_ALLOWED_HEADERS,
+  exposedHeaders: process.env.SECURE_CORS_EXPOSED_HEADERS,
+  credentials: process.env.SECURE_CORS_CREDENTIALS !== 'false',
+  preflightContinue: process.env.SECURE_CORS_PREFLIGHT_CONTINUE === 'true',
+};
+
 const app = express();
 
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.get('/health', (req, res) => {
