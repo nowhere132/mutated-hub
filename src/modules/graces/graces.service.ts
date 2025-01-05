63a7d49829cee9a18b29f28b1312904520fa0068
nowhere132 <canh.manh123@gmail.com>
2025-01-05 18:22:58 +0700

    [nowhere132][be] Define module graces

---
+++ src/modules/graces/graces.service.ts
@@ -0,0 +1,13 @@
+const collect = async (
+  link: string,
+  description: string | undefined,
+  tags: string[] | undefined,
+): Promise<number> => {
+  return Promise.resolve(1);
+};
+
+const enhance = async () => {};
+
+const show = async () => {};
+
+export { collect, enhance, show };
--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import * as gracesController from './modules/graces/graces.controller';
 
 const router = express.Router();
 
 router.get('/health', (req, res) => {
   res.send('OK');
 });
+
+router.post('/graces', gracesController.collect);
 
 export default router;
