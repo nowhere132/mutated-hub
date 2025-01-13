c1fe23561fcc0a1658a41e2515454177a5103abd
lanphgphm <lanphuongpham02@gmail.com>
2025-01-13 15:02:06 +0700

    [lanphgphm][be] Update grace's description or tags

--- src/router.ts
+++ src/router.ts
@@ -2,7 +2,9 @@ import express from 'express';
 import * as gracesController from './modules/graces/graces.controller';
 import { validateData } from './middlewares/validationMiddleware';
 import { collectGraceSchema } from './modules/graces/dto/collect-grace.dto';
-
+import { enhanceGraceSchema } from './modules/graces/dto/enhance-grace.dto'
+import { getPgPool } from './database/datasource';
+// import { showSchema } from ';
 const router = express.Router();
 
 router.get('/health', (req, res) => {
@@ -10,5 +12,29 @@ router.get('/health', (req, res) => {
 });
 
 router.post('/graces', validateData(collectGraceSchema), gracesController.collect);
+
+// i just gotta see what is going on :) 
+// nvm i could have just read the db schema
+router.get('/schema', async(req, res) => {
+  const pool = getPgPool(); 
+  try {
+    const query = `
+      select table_name, column_name, data_type 
+      from information_schema.columns 
+      where table_schema = 'public' 
+      order by table_name, ordinal_position; 
+    `;
+
+    const db = await pool.query(query);
+
+    res.send(db); 
+  } catch (e) {
+    console.error('cannot read db schema', e);
+    res.status(500).json({error: 'cannot read db schema'});
+  }
+});
+
+router.patch('/graces', validateData(enhanceGraceSchema), gracesController.enhance)
+
 
 export default router;
