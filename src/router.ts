46437440e6f4dce3b6abe873856bb4fd6cfad062
lanphgphm <lanphuongpham02@gmail.com>
2025-01-15 10:39:38 +0700

    [lanphgphm][be] Remove probing method that returns DB schema

--- src/router.ts
+++ src/router.ts
@@ -16,27 +16,6 @@ router.get('/graces/search', validateData(showGraceSchema), gracesController.sho
 
 router.post('/graces', validateData(collectGraceSchema), gracesController.collect);
 
-// i just gotta see what is going on :) 
-// nvm i could have just read the db schema
-router.get('/schema', async(req, res) => {
-  const pool = getPgPool(); 
-  try {
-    const query = `
-      select table_name, column_name, data_type 
-      from information_schema.columns 
-      where table_schema = 'public' 
-      order by table_name, ordinal_position; 
-    `;
-
-    const db = await pool.query(query);
-
-    res.send(db); 
-  } catch (e) {
-    console.error('cannot read db schema', e);
-    res.status(500).json({error: 'cannot read db schema'});
-  }
-});
-
 router.patch('/graces', validateData(enhanceGraceSchema), gracesController.enhance);
 
 export default router;
